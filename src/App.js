400777a503b693f2318d3ea5b46c8ca138c8946b
vikrambenipal <vikrambenipal@gmail.com>
2022-07-18 13:28:59 -0400

    to do list functionality complete

--- src/App.js
+++ src/App.js
@@ -10,9 +10,9 @@ function App() {
   const [search, setSearch] = useState("");
 
   useEffect(() => {
-    const use = axios.get('https://jsonplaceholder.typicode.com/users')
+    axios.get('https://jsonplaceholder.typicode.com/users')
     .then(res => setUsers(res.data));
-    const dos = axios.get('https://jsonplaceholder.typicode.com/todos')
+    axios.get('https://jsonplaceholder.typicode.com/todos')
     .then(res => setToDo(res.data));
   }, [])
 
@@ -21,7 +21,7 @@ function App() {
       <h1>ORX Tecnical Interview Challenge</h1>
       <h2>Vikram Benipal</h2>
       <Search setSearch={setSearch}/>
-      <User users={users} toDo={toDo} search={search} />
+      <User users={users} toDo={toDo} setToDo={setToDo} search={search} />
     </div>
   );
 }
--- src/components/User.js
+++ src/components/User.js
@@ -4,33 +4,74 @@ import styled from 'styled-components';
 const UserContainer = styled.div`
     border: 2px solid black;
 `
+const ToDoContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    margin-bottom: 10px;
+    border-bottom: 2px solid black;
+    p {
+        margin-left: 30px;
+    }
+    button {
+        color: white;
+        background-color: red;
+        font-size: 20px;
+        margin-right: 30px;
+        margin-top: 5px;
+        margin-bottom: 10px;
+        cursor: pointer;
+    }
+`
 
-const User = ({ users, toDo, search }) => {
+const ItemText = styled.p`
+    cursor: pointer;
+    text-decoration: ${props => props.completed ? 'line-through' : 'none'}
+
+`
+const User = ({ users, toDo, setToDo, search }) => {
+
+    const toggleItem = (e) => {
+        const id = e.target.id;
+        let newToDos = toDo;
+        const index = newToDos.findIndex(item => item.id == id);
+        newToDos[index].completed = !newToDos[index].completed;
+        setToDo([...newToDos]);
+    }
+
+    const handleDelete = (e) => {
+        const id = e.target.id;
+        console.log(id)
+        const newToDos = toDo.filter(item => item.id != id);
+        setToDo([...newToDos]);
+    }
+
   return (
     <div>
         {users.filter(user => (user.name.includes(search) || user.email.includes(search))).map((user,key) => {
         return (
           <div>
             <UserContainer key={key}>
-                <p>{user.name}</p>
+                <h3>{user.name}</h3>
                 <p>{user.email}</p>
                 <p>{user.phone}</p>
                 <p>{user.address.street + ", " + user.address.suite + ", " + user.address.city}</p>
                 <p>{user.website}</p>
-                <h2>To Do Items:</h2>
+                <h3>To Do Items:</h3>
                 {toDo.map(item => (
                     item.userId == user.id ? (
-                        <div>
-                            <p>{item.title}</p>
-                            <p>Completed: {item.completed ? "Complete!" : "Incomplete"}</p>
-                        </div>
+                        <ToDoContainer>
+                            <div>
+                                <ItemText completed={item.completed} id={item.id} onClick={toggleItem}>{item.title}</ItemText>
+                                <p>Completed: {item.completed ? "Complete!" : "Incomplete"}</p>
+                            </div>
+                            <button id={item.id} onClick={handleDelete}>DELETE</button>
+                        </ToDoContainer>
                     ) : <Fragment/>
                 ))}
-
             </UserContainer>
             
           </div>  
-          
         )
       })}
     </div>
